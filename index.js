44a2585e1b5f86b725d0c66b1e32763acdc78b67
Manish Singh <manishsingh04031998@gmail.com>
2023-07-15 11:34:18 +0530

    SetUp-Express-Session

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
-
+const port = 8000;
 // require cookie
 const cookieParser = require('cookie-parser');
-
 const db = require('./config/mongoose');
-const port = 8000;
+const expressLayouts = require('express-ejs-lauouts');
+
+// used for session cookie
+const session = require('express-session');
+const passport =  require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+
 // cookie
 app.use(cookieParser());
 
@@ -18,6 +23,22 @@ app.use('/',require('./routes'));
 // setup view engin
 app.set('view engine','ejs');
 app.set('views','./views');
+
+
+app.use(session({
+    name:'social',
+    // TODO change the secret before deployment in production mode
+    secret:"somethig",
+    saveUninitialized:false,
+    resave:false,
+    cookie:{
+        maxAge:(1000 * 60 * 100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // reading static file
 app.use(express.static('assets'));
