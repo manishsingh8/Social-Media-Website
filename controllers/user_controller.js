b7f0d91eeac0cf8d32345fa394687665493cd734
Manish Singh <manishsingh04031998@gmail.com>
2023-07-17 15:51:28 +0530

    Remove-Manual-Authentication

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,20 +4,23 @@ const User = require('../models/user');
 
 // renders users profile page
 module.exports.profile = function (req, res) {
-  console.log(req.cookies.user_id);
-  if(req.cookies.user_id){
-    User.findById(req.cookies.user_id)
-     .then((dbUser)=>{
-          return res.render("profile", {
-            title: "Profile",
-            user:dbUser
-          });
-        }).catch((err)=>{
-          return res.redirect('/users/signin');
-     })
-  }else{
-   return res.redirect('/users/signin');
-  }
+  // console.log(req.cookies.user_id);
+  // if(req.cookies.user_id){
+  //   User.findById(req.cookies.user_id)
+  //    .then((dbUser)=>{
+  //         return res.render("profile", {
+  //           title: "Profile",
+  //           user:dbUser
+  //         });
+  //       }).catch((err)=>{
+  //         return res.redirect('/users/signin');
+  //    })
+  // }else{
+  //  return res.redirect('/users/signin');
+  // }
+  return res.render('profile',{
+    title:'User Profile ' 
+  })
 };
 
 //  renders users signup page
@@ -64,38 +67,18 @@ module.exports.create =  function(req,res){
     })
 }
 
-// get the signin and create the session
-// module.exports.createSession =  function(req,res){
-// //  find the user
-//       User.findOne({email:req.body.email})
-//       // handle user found
-//       .then((user)=>{
-//           if(user.password != req.body.password){
-//             return res.redirect("back");
-//           }
-//           // handle session creation
-//           res.cookie('user_id',user.id);
-//           res.redirect('/users/profile');
-
-//       }).catch((err)=>{
-//         // handle user not found
-//         console.log("Error while finding the user to signin");
-//         return res.redirect('back');
-//       })
-// }
-
 // get the signin and create the session using passport
 module.exports.createSession =  function(req,res){
-     return res.redirect("/");
+     return res.redirect("/");// after user get signind we will redirect user to home page
 }
 
 
 
-// signout
-module.exports.signout = function(req,res){
-      //  cleare the user_id after user click on signout
-      console.log(req.cookies.user_id);
-      res.clearCookie('user_id'); 
-      console.log("user_id is removed from cookie");
-    return res.redirect('/users/signin');
-}
+// // signout
+// module.exports.signout = function(req,res){
+//       //  cleare the user_id after user click on signout
+//       console.log(req.cookies.user_id);
+//       res.clearCookie('user_id'); 
+//       console.log("user_id is removed from cookie");
+//     return res.redirect('/users/signin');
+// }
